5ebdca1c7c0cd252f1dbb630f3eed59e2c2417fe
UPYOG-GIT <supoort@entitcs.com>
2023-01-12 17:53:46 +0530

    add log for error in BPA-Calculator

--- municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/services/MDMSService.java
+++ municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/services/MDMSService.java
@@ -36,79 +36,78 @@ import net.minidev.json.JSONArray;
 public class MDMSService {
 
 	@Autowired
-	 private ServiceRequestRepository serviceRequestRepository;
+	private ServiceRequestRepository serviceRequestRepository;
 
 	@Autowired
 	private BPACalculatorConfig config;
 
 	@Autowired
 	private EDCRService edcrService;
-	
-    public Object mDMSCall(CalculationReq calculationReq,String tenantId){
-        MdmsCriteriaReq mdmsCriteriaReq = getMDMSRequest(calculationReq,tenantId);
-        StringBuilder url = getMdmsSearchUrl();
-        Object result = serviceRequestRepository.fetchResult(url , mdmsCriteriaReq);
-        return result;
-    }
 
-    /**
-     * Creates and returns the url for mdms search endpoint
-     *
-     * @return MDMS Search URL
-     */
-    private StringBuilder getMdmsSearchUrl() {
-        return new StringBuilder().append(config.getMdmsHost()).append(config.getMdmsSearchEndpoint());
-    }
+	public Object mDMSCall(CalculationReq calculationReq, String tenantId) {
+		MdmsCriteriaReq mdmsCriteriaReq = getMDMSRequest(calculationReq, tenantId);
+		StringBuilder url = getMdmsSearchUrl();
+		Object result = serviceRequestRepository.fetchResult(url, mdmsCriteriaReq);
+		return result;
+	}
 
-    /**
-     * Creates MDMS request
-     * @param requestInfo The RequestInfo of the calculationRequest
-     * @param tenantId The tenantId of the tradeLicense
-     * @return MDMSCriteria Request
-     */
-    private MdmsCriteriaReq getMDMSRequest(CalculationReq calculationReq, String tenantId) {
-    	RequestInfo requestInfo = 	calculationReq.getRequestInfo();
-        List<MasterDetail> bpaMasterDetails = new ArrayList<>();
-        
-        bpaMasterDetails.add(MasterDetail.builder().name(BPACalculatorConstants.MDMS_CALCULATIONTYPE)
-        		.build());
-        ModuleDetail bpaModuleDtls = ModuleDetail.builder().masterDetails(bpaMasterDetails)
-                .moduleName(BPACalculatorConstants.MDMS_BPA).build();
+	/**
+	 * Creates and returns the url for mdms search endpoint
+	 *
+	 * @return MDMS Search URL
+	 */
+	private StringBuilder getMdmsSearchUrl() {
+		return new StringBuilder().append(config.getMdmsHost()).append(config.getMdmsSearchEndpoint());
+	}
 
-        List<ModuleDetail> moduleDetails = new ArrayList<>();
-        
-        moduleDetails.add(bpaModuleDtls);
+	/**
+	 * Creates MDMS request
+	 * 
+	 * @param requestInfo The RequestInfo of the calculationRequest
+	 * @param tenantId    The tenantId of the tradeLicense
+	 * @return MDMSCriteria Request
+	 */
+	private MdmsCriteriaReq getMDMSRequest(CalculationReq calculationReq, String tenantId) {
+		RequestInfo requestInfo = calculationReq.getRequestInfo();
+		List<MasterDetail> bpaMasterDetails = new ArrayList<>();
 
-        MdmsCriteria mdmsCriteria = MdmsCriteria.builder().moduleDetails(moduleDetails).tenantId(tenantId)
-                .build();
+		bpaMasterDetails.add(MasterDetail.builder().name(BPACalculatorConstants.MDMS_CALCULATIONTYPE).build());
+		ModuleDetail bpaModuleDtls = ModuleDetail.builder().masterDetails(bpaMasterDetails)
+				.moduleName(BPACalculatorConstants.MDMS_BPA).build();
 
-        return MdmsCriteriaReq.builder().requestInfo(requestInfo).mdmsCriteria(mdmsCriteria).build();
-    }
+		List<ModuleDetail> moduleDetails = new ArrayList<>();
 
+		moduleDetails.add(bpaModuleDtls);
 
-    /**
-     * Gets the calculationType for the city for a particular financialYear
-     * If for particular financialYear entry is not there previous year is taken
-     * If MDMS data is not available default values are returned
-     * @param requestInfo The RequestInfo of the calculationRequest
-     * @param license The tradeLicense for which calculation is done
-     * @return Map contianing the calculationType for TradeUnit and accessory
-     */
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-  public Map getCalculationType(RequestInfo requestInfo,BPA bpa,Object mdmsData, String feeType){
-         HashMap<String,Object> calculationType = new HashMap<>();
-         try {
-             List jsonOutput = JsonPath.read(mdmsData, BPACalculatorConstants.MDMS_CALCULATIONTYPE_PATH);
-             LinkedHashMap responseMap = edcrService.getEDCRDetails(requestInfo, bpa);
-             
-             log.info("jsonOutput logg :======= "+jsonOutput);
-             
-             String jsonString = new JSONObject(responseMap).toString();
-      DocumentContext context = JsonPath.using(Configuration.defaultConfiguration()).parse(jsonString);
-      Map<String, String> additionalDetails = new HashMap<String, String>();
-     
-      log.info("context logg:======= "+context);
-     
+		MdmsCriteria mdmsCriteria = MdmsCriteria.builder().moduleDetails(moduleDetails).tenantId(tenantId).build();
+
+		return MdmsCriteriaReq.builder().requestInfo(requestInfo).mdmsCriteria(mdmsCriteria).build();
+	}
+
+	/**
+	 * Gets the calculationType for the city for a particular financialYear If for
+	 * particular financialYear entry is not there previous year is taken If MDMS
+	 * data is not available default values are returned
+	 * 
+	 * @param requestInfo The RequestInfo of the calculationRequest
+	 * @param license     The tradeLicense for which calculation is done
+	 * @return Map contianing the calculationType for TradeUnit and accessory
+	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public Map getCalculationType(RequestInfo requestInfo, BPA bpa, Object mdmsData, String feeType) {
+		HashMap<String, Object> calculationType = new HashMap<>();
+		try {
+			List jsonOutput = JsonPath.read(mdmsData, BPACalculatorConstants.MDMS_CALCULATIONTYPE_PATH);
+			LinkedHashMap responseMap = edcrService.getEDCRDetails(requestInfo, bpa);
+
+			log.info("jsonOutput logg :======= " + jsonOutput);
+
+			String jsonString = new JSONObject(responseMap).toString();
+			DocumentContext context = JsonPath.using(Configuration.defaultConfiguration()).parse(jsonString);
+			Map<String, String> additionalDetails = new HashMap<String, String>();
+
+			log.info("context logg:======= " + context);
+
 //     JSONArray serviceType = context.read("edcrDetail.*.applicationSubType");
 //     if (CollectionUtils.isEmpty(serviceType)) {
 //     serviceType.add("NEW_CONSTRUCTION");
@@ -118,21 +117,21 @@ public class MDMSService {
 //     applicationType.add("permit");
 //     }
 //     additionalDetails.put("applicationType", applicationType.get(0).toString());
- 
-      JSONArray occupancyType = context.read("edcrDetail[0].planDetail.planInformation.occupancy");
-     
-      JSONArray totalBuitUpArea = context.read("edcrDetail[0].planDetail.virtualBuilding.totalBuitUpArea");
-     
-      additionalDetails.put("occupancyType", occupancyType.get(0).toString());
-      additionalDetails.put("totalBuitUpArea", totalBuitUpArea.get(0).toString());
-     
-      log.info("JSONArray occupancyType ki value :--------- "+occupancyType.get(0).toString()+"=========JSONArray totalBuitUpArea ki value:----"+totalBuitUpArea.get(0).toString());
-             log.info("occupancyType form additional=====:  "+additionalDetails.get("occupancyType"));
-             log.info("totalBuitUpArea form additional=====:  "+additionalDetails.get("totalBuitUpArea"));
+
+			JSONArray occupancyType = context.read("edcrDetail[0].planDetail.planInformation.occupancy");
+
+			JSONArray totalBuitUpArea = context.read("edcrDetail[0].planDetail.virtualBuilding.totalBuitUpArea");
+
+			additionalDetails.put("occupancyType", occupancyType.get(0).toString());
+			additionalDetails.put("totalBuitUpArea", totalBuitUpArea.get(0).toString());
+
+			log.info("JSONArray occupancyType ki value :--------- " + occupancyType.get(0).toString()
+					+ "=========JSONArray totalBuitUpArea ki value:----" + totalBuitUpArea.get(0).toString());
+			log.info("occupancyType form additional=====:  " + additionalDetails.get("occupancyType"));
+			log.info("totalBuitUpArea form additional=====:  " + additionalDetails.get("totalBuitUpArea"));
 //             log.debug("applicationType is " + additionalDetails.get("applicationType"));
 //             log.debug("serviceType is " + additionalDetails.get("serviceType"));
-             
-             
+
 //             String filterExp = "$.[?((@.applicationType == '"+ additionalDetails.get("applicationType")+"' || @.applicationType === 'ALL' ) &&  @.feeType == '"+feeType+"')]";
 //             List<Object> calTypes = JsonPath.read(jsonOutput, filterExp);
 //            
@@ -141,62 +140,59 @@ public class MDMSService {
 //            
 //             filterExp = "$.[?(@.riskType == '"+bpa.getRiskType()+"' || @.riskType === 'ALL' )]";
 //             calTypes = JsonPath.read(calTypes, filterExp);
-             
-             
-//             ----added by manisha for filter amount-------
-             String filterExp = "$.[?(@.occupancyType == '"+additionalDetails.get("occupancyType")+"')]";
-             log.info("filterExp:------ "+filterExp);
-             List<Object> calTypes = JsonPath.read(jsonOutput, filterExp);
-             
-             filterExp = "$.[?(@.builtupAreaFrom <="+ additionalDetails.get("totalBuitUpArea")+"&& @.builtupAreaTo >="+additionalDetails.get("totalBuitUpArea")+")]";
-             log.info("filterExp:------ "+filterExp);
-             calTypes = JsonPath.read(calTypes, filterExp);
-             
-             
-             
-//             ----added by manisha for filter amount-------
-             
-             
-             
-             if(calTypes.size() > 1){
-              filterExp = "$.[?(@.riskType == '"+bpa.getRiskType()+"' )]";
-              calTypes  = JsonPath.read(calTypes, filterExp);
-             }
-             
-             if(calTypes.size() == 0) {
-            log.info("================should not enter==========");
-              return defaultMap(feeType);
-             }
-             
-             log.info("calTypes--------------"+calTypes+"==========calTypes.get(0)=========="+calTypes.get(0));
-              Object obj = calTypes.get(0);
-           
-             int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-             
-            String financialYear = currentYear + "-" + (currentYear + 1);
-            System.out.println(financialYear);
-             
-             calculationType = (HashMap<String, Object>) obj;
-         }
-         catch (Exception e){
-             throw new CustomException(BPACalculatorConstants.CALCULATION_ERROR, "Failed to get calculationType");
-         }
-         
-         log.info("calculationType-----------"+calculationType);
-         return calculationType;
-     } 
 
-   
-    /**
-     * Creates and return default calculationType values as map
-     * @return default calculationType Map
-     */	
-    private Map defaultMap(String feeType){
-        Map defaultMap = new HashMap();
-        String feeAmount = ( feeType.equalsIgnoreCase(BPACalculatorConstants.MDMS_CALCULATIONTYPE_APL_FEETYPE) ) ? config.getApplFeeDefaultAmount() : config.getSancFeeDefaultAmount();
-        defaultMap.put( BPACalculatorConstants.MDMS_CALCULATIONTYPE_AMOUNT,feeAmount);
-        return defaultMap;
-    }
+//             ----added by manisha for filter amount-------
+			String filterExp = "$.[?(@.occupancyType == '" + additionalDetails.get("occupancyType") + "')]";
+			log.info("filterExp:------ " + filterExp);
+			List<Object> calTypes = JsonPath.read(jsonOutput, filterExp);
 
+			filterExp = "$.[?(@.builtupAreaFrom <=" + additionalDetails.get("totalBuitUpArea") + "&& @.builtupAreaTo >="
+					+ additionalDetails.get("totalBuitUpArea") + ")]";
+			log.info("filterExp:------ " + filterExp);
+			calTypes = JsonPath.read(calTypes, filterExp);
+
+//             ----added by manisha for filter amount-------
+
+			if (calTypes.size() > 1) {
+				filterExp = "$.[?(@.riskType == '" + bpa.getRiskType() + "' )]";
+				calTypes = JsonPath.read(calTypes, filterExp);
+			}
+
+			if (calTypes.size() == 0) {
+				log.info("================should not enter==========");
+				return defaultMap(feeType);
+			}
+
+			log.info("calTypes--------------" + calTypes + "==========calTypes.get(0)==========" + calTypes.get(0));
+			Object obj = calTypes.get(0);
+
+			int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+
+			String financialYear = currentYear + "-" + (currentYear + 1);
+			System.out.println(financialYear);
+
+			calculationType = (HashMap<String, Object>) obj;
+		} catch (Exception e) {
+			log.error("Exception :" + e);
+			throw new CustomException(BPACalculatorConstants.CALCULATION_ERROR, "Failed to get calculationType");
+		}
+
+		log.info("calculationType-----------" + calculationType);
+		return calculationType;
+	}
+
+	/**
+	 * Creates and return default calculationType values as map
+	 * 
+	 * @return default calculationType Map
+	 */
+	private Map defaultMap(String feeType) {
+		Map defaultMap = new HashMap();
+		String feeAmount = (feeType.equalsIgnoreCase(BPACalculatorConstants.MDMS_CALCULATIONTYPE_APL_FEETYPE))
+				? config.getApplFeeDefaultAmount()
+				: config.getSancFeeDefaultAmount();
+		defaultMap.put(BPACalculatorConstants.MDMS_CALCULATIONTYPE_AMOUNT, feeAmount);
+		return defaultMap;
+	}
 
 }
