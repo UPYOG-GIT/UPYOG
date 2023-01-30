8e986493575ea6dda62c3970307a9b44831d89ab
UPYOG-GIT <supoort@entitcs.com>
2023-01-30 10:56:58 +0530

    BPA Service addCalculation() changes

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/service/BPAService.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/service/BPAService.java
@@ -186,13 +186,15 @@ public class BPAService {
 	 */
 	private void addCalculation(String applicationType, BPARequest bpaRequest) {
 
-//		if (bpaRequest.getBPA().getRiskType().equals(BPAConstants.LOW_RISKTYPE)
-//				&& !applicationType.equalsIgnoreCase(BPAConstants.BUILDING_PLAN_OC)) {
-////			calculationService.addCalculation(bpaRequest, BPAConstants.LOW_RISK_PERMIT_FEE_KEY);
-//			calculationService.addCalculation(bpaRequest, BPAConstants.LOW_APPLICATION_FEE_KEY);
-//		} else {
+		log.info("inside BPAService.addCalculation()........");
+		log.info("applicationType: " + applicationType);
+		if (bpaRequest.getBPA().getRiskType().equals(BPAConstants.LOW_RISKTYPE)
+				&& !applicationType.equalsIgnoreCase(BPAConstants.BUILDING_PLAN_OC)) {
+//			calculationService.addCalculation(bpaRequest, BPAConstants.LOW_RISK_PERMIT_FEE_KEY);
+			calculationService.addCalculation(bpaRequest, BPAConstants.LOW_APPLICATION_FEE_KEY);
+		} else {
 			calculationService.addCalculation(bpaRequest, BPAConstants.APPLICATION_FEE_KEY);
-//		}
+		}
 	}
 
 	/**
