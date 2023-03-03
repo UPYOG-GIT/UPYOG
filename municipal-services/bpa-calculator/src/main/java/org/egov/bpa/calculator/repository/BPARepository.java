88746ead3bd5f195b39f89581b1276f90cd75fac
unknown <manishayadu3766@gmail.com>
2023-03-03 10:40:11 +0530

    add log in bpaRepository in bpaCalculator

--- municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/repository/BPARepository.java
+++ municipal-services/bpa-calculator/src/main/java/org/egov/bpa/calculator/repository/BPARepository.java
@@ -7,21 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Repository
+@Slf4j
 public class BPARepository {
 
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	
+
 	public String[] getSanctionFeeAmount(String consumerCode) {
-		String sql="select totalamount from egbs_billdetail_v1 where businessservice ='BPA.NC_SAN_FEE' and consumercode =?";
+		String sql = "select totalamount from egbs_billdetail_v1 where businessservice ='BPA.NC_SAN_FEE' and consumercode =?";
 		return jdbcTemplate.queryForObject(sql, new Object[] { consumerCode }, String[].class);
 //		return null;
 	}
-	
-	public List<Map<String,Object>> getPaytyDate(String feety, String tenantid) {
-		String sql = "select id, charges_type_name,zdaflg from paytype_master where ulb_tenantid=? and payment_type=? and defunt='N' and optflag='N'"; 
-		return jdbcTemplate.queryForList(sql, new Object[] { tenantid, feety} );
-		
+
+	public List<Map<String, Object>> getPaytyDate(String feetype, String tenantid) {
+//		String sql = "select id, charges_type_name,zdaflg from paytype_master where ulb_tenantid=? and payment_type=? and defunt='N' and optflag='N'"; 
+		String sql = "select id, charges_type_name,zdaflg from paytype_master where ulb_tenantid='" + tenantid
+				+ "' and payment_type='" + feetype + "' and defunt='N' and optflag='N'";
+		log.info("BPARepository.getPaytyDate Query : " + sql);
+//		return jdbcTemplate.queryForList(sql, new Object[] { tenantid, feetype });
+		return jdbcTemplate.queryForList(sql);
+
 	}
 }
