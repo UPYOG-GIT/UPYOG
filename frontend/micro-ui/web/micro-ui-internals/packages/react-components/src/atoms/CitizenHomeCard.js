72274eec0a3a8f93832cba5e532fd24e26816ae7
UPYOG-GIT <support@entitcs.com>
2023-03-10 14:06:05 +0530

    changes of dashboard

--- frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/CitizenHomeCard.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/react-components/src/atoms/CitizenHomeCard.js
@@ -4,17 +4,22 @@ import { Link } from "react-router-dom";
 const CitizenHomeCard = ({ header, links, state, Icon, Info, isInfo = false, styles }) => {
   return (
     <div className="CitizenHomeCard" style={styles ? styles : {}}>
+
       <div className="header">
         <h2>{header}</h2>
         <Icon />
       </div>
 
       <div className="links">
+       
         {links.map((e, i) => (
           <Link key={i} to={{ pathname: e.link, state: e.state }}>
             {e.i18nKey}
           </Link>
         ))}
+      </div>
+      <div className="links" style={{fontSize: 14}}>
+        <a href="/digit-ui/citizen/obps/stakeholder/apply/stakeholder-docs-required">Stakeholder Registration</a>
       </div>
       <div>
         {isInfo ? <Info /> : null} 
