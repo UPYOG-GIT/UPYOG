da7ca80e4a28224d64b400d4b50f5a6912a7e7b8
UPYOG-GIT <supoort@entitcs.com>
2023-03-15 12:11:54 +0530

    SubOccupancy Added into table

---
+++ edcr/service/egov/egov-commons/src/main/resources/db/migration/main/V20230315121547__commons_add_SubOccupancy.sql
@@ -0,0 +1,2 @@
+
+INSERT INTO egbpa_sub_occupancy(id, code, name, ordernumber, isactive, createdby, createddate, lastmodifieddate, lastmodifiedby, version, maxcoverage, minfar, maxfar, occupancy, description, colorcode)  VALUES (nextval('seq_egbpa_sub_occupancy'), 'F', 'Mercantile / Commercial', 63, 't', 1, now(), now(), 1, 0,10, 15, 0, (select id from egbpa_occupancy where code='F'), 'Mercantile / Commercial', 7);
