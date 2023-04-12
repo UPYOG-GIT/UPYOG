0458e876e451e7f3eadf99656f0daea9e493e703
UPYOG-GIT <supoort@entitcs.com>
2023-04-12 11:19:26 +0530

    Changed in start.sh

--- municipal-services/bpa-calculator/start.sh
+++ municipal-services/bpa-calculator/start.sh
@@ -4,7 +4,7 @@ if [[ -z "${JAVA_OPTS}" ]];then
     export JAVA_OPTS="-Xmx64m -Xms64m"
 fi
 
-java_debug_args="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=${JAVA_DEBUG_PORT:-5005}"
+java_debug_args="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005"
 
 
 exec java ${java_debug_args} ${JAVA_OPTS} ${JAVA_ARGS}  -jar /opt/egov/*.jar
