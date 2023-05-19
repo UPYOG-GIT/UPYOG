40c3678ed911c1c655e01392625121a46a22056b
UPYOG-GIT <supoort@entitcs.com>
2023-05-19 16:32:09 +0530

    USER role for stackholder state level

--- core-services/egov-user/src/main/java/org/egov/user/web/contract/RoleRequest.java
+++ core-services/egov-user/src/main/java/org/egov/user/web/contract/RoleRequest.java
@@ -9,24 +9,27 @@ import org.egov.user.domain.model.Role;
 @Getter
 @Builder
 @AllArgsConstructor
-@EqualsAndHashCode(of = {"code", "tenantId"})
+@EqualsAndHashCode(of = { "code", "tenantId" })
 public class RoleRequest {
 
-    private String code;
-    private String name;
-    private String tenantId;
+	private String code;
+	private String name;
+	private String tenantId;
 
-    public RoleRequest(Role domainRole) {
-        this.code = domainRole.getCode();
-        this.name = domainRole.getName();
-        this.tenantId = domainRole.getTenantId();
-    }
+	public RoleRequest(Role domainRole) {
+		this.code = domainRole.getCode();
+		this.name = domainRole.getName();
+		this.tenantId = (domainRole.getCode().equals("CITIZEN") || domainRole.getCode().equals("BPA_ARCHITECT")
+				|| domainRole.getCode().equals("BPA_BUILDER") || domainRole.getCode().equals("BPA_ENGINEER")
+				|| domainRole.getCode().equals("BPA_STRUCTURALENGINEER")
+				|| domainRole.getCode().equals("BPA_SUPERVISOR") || domainRole.getCode().equals("BPA_TOWNPLANNER"))
+						? domainRole.getTenantId().split("\\.")[0]
+						: domainRole.getTenantId();
+//		this.tenantId = domainRole.getTenantId();
 
-    public Role toDomain() {
-        return Role.builder()
-                .code(code)
-                .name(name)
-                .tenantId(tenantId)
-                .build();
-    }
+	}
+
+	public Role toDomain() {
+		return Role.builder().code(code).name(name).tenantId(tenantId).build();
+	}
 }
