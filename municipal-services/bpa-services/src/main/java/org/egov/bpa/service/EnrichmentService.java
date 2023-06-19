499e4bbf50182059ef5f3f4c37626f1ab7978093
UPYOG-GIT <support@entitcs.com>
2023-06-19 16:18:04 +0530

    changes for vlow risktype, keeping it inside BPA_LOW

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/service/EnrichmentService.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/service/EnrichmentService.java
@@ -101,7 +101,8 @@ public class EnrichmentService {
 		bpaRequest.getBPA().setAccountId(bpaRequest.getBPA().getAuditDetails().getCreatedBy());
 		String applicationType = values.get(BPAConstants.APPLICATIONTYPE);
 		if (applicationType.equalsIgnoreCase(BPAConstants.BUILDING_PLAN)) {
-			if (!bpaRequest.getBPA().getRiskType().equalsIgnoreCase(BPAConstants.LOW_RISKTYPE)) {
+			if (!bpaRequest.getBPA().getRiskType().equalsIgnoreCase(BPAConstants.LOW_RISKTYPE)
+				    || !bpaRequest.getBPA().getRiskType().equalsIgnoreCase(BPAConstants.VLOW_RISKTYPE)){
 				bpaRequest.getBPA().setBusinessService(BPAConstants.BPA_MODULE_CODE);
 			} else {
 				bpaRequest.getBPA().setBusinessService(BPAConstants.BPA_LOW_MODULE_CODE);
