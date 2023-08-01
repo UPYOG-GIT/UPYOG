1539bd14d82b6197f1781ddae5e6ab11f8d7c919
UPYOG-GIT <supoort@entitcs.com>
2023-08-01 12:44:12 +0530

    RGA Service Flyway removed from dependency

--- municipal-services/rga-services/src/main/java/org/entit/rga/RegularisationServiceApplication.java
+++ municipal-services/rga-services/src/main/java/org/entit/rga/RegularisationServiceApplication.java
@@ -1,9 +1,12 @@
 package org.entit.rga;
 
+import org.egov.tracer.config.TracerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({ TracerConfiguration.class })
 public class RegularisationServiceApplication {
 
 	public static void main(String[] args) {
