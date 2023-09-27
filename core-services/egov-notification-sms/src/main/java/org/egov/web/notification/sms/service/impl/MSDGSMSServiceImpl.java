8ae781ed22e2ac34e45fc7a664f492edcd965ee0
UPYOG-GIT <supoort@entitcs.com>
2023-09-27 17:01:07 +0530

    SMS Service Changes

--- core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/service/impl/MSDGSMSServiceImpl.java
+++ core-services/egov-notification-sms/src/main/java/org/egov/web/notification/sms/service/impl/MSDGSMSServiceImpl.java
@@ -26,66 +26,66 @@ import java.util.Map;
 @ConditionalOnProperty(value = "sms.provider.class", matchIfMissing = true, havingValue = "MSDG")
 public class MSDGSMSServiceImpl extends BaseSMSService {
 
-    @Autowired
-    private SMSProperties smsProperties;
+	@Autowired
+	private SMSProperties smsProperties;
 
-    @Autowired
-    private SMSBodyBuilder bodyBuilder;
+	@Autowired
+	private SMSBodyBuilder bodyBuilder;
 
+	/**
+	 * MD5 encryption algorithm
+	 *
+	 * @param text
+	 * @return
+	 */
+	private static String MD5(String text) {
+		MessageDigest md;
+		byte[] md5 = new byte[64];
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+			md.update(text.getBytes("iso-8859-1"), 0, text.length());
+			md5 = md.digest();
+		} catch (Exception e) {
+			log.error("Exception while encrypting the pwd: ", e);
+		}
+		return convertedToHex(md5);
 
-    /**
-     * MD5 encryption algorithm
-     *
-     * @param text
-     * @return
-     */
-    private static String MD5(String text) {
-        MessageDigest md;
-        byte[] md5 = new byte[64];
-        try {
-            md = MessageDigest.getInstance("SHA-256");
-            md.update(text.getBytes("iso-8859-1"), 0, text.length());
-            md5 = md.digest();
-        } catch (Exception e) {
-            log.error("Exception while encrypting the pwd: ", e);
-        }
-        return convertedToHex(md5);
+	}
 
-    }
+	private static String convertedToHex(byte[] data) {
+		StringBuffer buf = new StringBuffer();
 
-    private static String convertedToHex(byte[] data) {
-        StringBuffer buf = new StringBuffer();
+		for (int i = 0; i < data.length; i++) {
+			int halfOfByte = (data[i] >>> 4) & 0x0F;
+			int twoHalfBytes = 0;
 
-        for (int i = 0; i < data.length; i++) {
-            int halfOfByte = (data[i] >>> 4) & 0x0F;
-            int twoHalfBytes = 0;
+			do {
+				if (0 <= halfOfByte && halfOfByte <= 9)
+					buf.append((char) ('0' + halfOfByte));
+				else
+					buf.append((char) ('a' + (halfOfByte - 10)));
 
-            do {
-                if (0 <= halfOfByte && halfOfByte <= 9)
-                    buf.append((char) ('0' + halfOfByte));
-                else
-                    buf.append((char) ('a' + (halfOfByte - 10)));
+				halfOfByte = data[i] & 0x0F;
 
-                halfOfByte = data[i] & 0x0F;
+			} while (twoHalfBytes++ < 1);
+		}
+		return buf.toString();
+	}
 
-            } while (twoHalfBytes++ < 1);
-        }
-        return buf.toString();
-    }
-
-    protected void submitToExternalSmsService(Sms sms) {
+	protected void submitToExternalSmsService(Sms sms) {
 //        String finalmessage = "";
-    	String[] msgTemplate=sms.getMessage().split("##");
+		String[] msgTemplate = sms.getMessage().split("##");
 //        String finalmessage = sms.getMessage();
-    	if(msgTemplate.length==2) {
+		if (msgTemplate.length == 2) {
 //    		finalmessage=msgTemplate[0];
-    		sms.setMessage(msgTemplate[0].trim());
-    		sms.setTemplateId(msgTemplate[1].trim());
-    	}else {
+			sms.setMessage(msgTemplate[0].trim());
+			sms.setTemplateId(msgTemplate[1].trim());
+		} else {
 //    		finalmessage=msgTemplate[0];
-    		sms.setMessage(msgTemplate[0].trim());
-    		sms.setTemplateId(smsProperties.getSmsDefaultTmplid());
-    	}
+			sms.setMessage(msgTemplate[0].trim());
+			sms.setTemplateId(smsProperties.getSmsDefaultTmplid());
+		}
+		log.info("Message : " + sms.getMessage());
 //        for (int i = 0; i < sms.getMessage().length(); i++) {
 //            char ch = sms.getMessage().charAt(i);
 //            int j = (int) ch;
@@ -93,111 +93,111 @@ public class MSDGSMSServiceImpl extends BaseSMSService {
 //            finalmessage = finalmessage + sss;
 //        }
 //        sms.setMessage(finalmessage);
-        String url = smsProperties.getUrl();
-        final MultiValueMap<String, String> requestBody = bodyBuilder.getSmsRequestBody(sms);
-        postProcessor(requestBody);
-        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(requestBody, getHttpHeaders());
-        executeAPI(URI.create(url), HttpMethod.POST, request, String.class);
-    }
+		String url = smsProperties.getUrl();
+		final MultiValueMap<String, String> requestBody = bodyBuilder.getSmsRequestBody(sms);
+		postProcessor(requestBody);
+		HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(requestBody, getHttpHeaders());
+		executeAPI(URI.create(url), HttpMethod.POST, request, String.class);
+	}
 
-    /**
-     * Performs post processing on the default parameters
-     *
-     * @param requestBody
-     */
-    private void postProcessor(MultiValueMap<String, String> requestBody) {
-        Map<String, String> configMap = getConfigMap();
-        String password = requestBody.getFirst(configMap.get(SMSConstants.SENDER_PASSWORD_IDENTIFIER));
-        String username = requestBody.getFirst(configMap.get(SMSConstants.SENDER_USERNAME_IDENTIFIER));
-        String senderid = requestBody.getFirst(configMap.get(SMSConstants.SENDER_SENDERID_IDENTIFIER));
-        String message = requestBody.getFirst(configMap.get(SMSConstants.SENDER_MESSAGE_IDENTIFIER));
-        String secureKey = requestBody.getFirst(configMap.get(SMSConstants.SENDER_SECUREKEY_IDENTIFIER));
-        String templateId = requestBody.getFirst(configMap.get(SMSConstants.TEMPLATE_ID));
+	/**
+	 * Performs post processing on the default parameters
+	 *
+	 * @param requestBody
+	 */
+	private void postProcessor(MultiValueMap<String, String> requestBody) {
+		Map<String, String> configMap = getConfigMap();
+		String password = requestBody.getFirst(configMap.get(SMSConstants.SENDER_PASSWORD_IDENTIFIER));
+		String username = requestBody.getFirst(configMap.get(SMSConstants.SENDER_USERNAME_IDENTIFIER));
+		String senderid = requestBody.getFirst(configMap.get(SMSConstants.SENDER_SENDERID_IDENTIFIER));
+		String message = requestBody.getFirst(configMap.get(SMSConstants.SENDER_MESSAGE_IDENTIFIER));
+		String secureKey = requestBody.getFirst(configMap.get(SMSConstants.SENDER_SECUREKEY_IDENTIFIER));
+		String templateId = requestBody.getFirst(configMap.get(SMSConstants.TEMPLATE_ID));
 
 //        String encryptedPwd = MD5(password);
-        String encryptedPwd = password;
-        String hashMsg = hashGenerator(username, senderid, message, secureKey);
+		String encryptedPwd = password;
+		String hashMsg = hashGenerator(username, senderid, message, secureKey);
 
-        List<String> entriesToBeModified = new ArrayList<>();
-        for (String key : requestBody.keySet()) {
-            if (key.equals(configMap.get(SMSConstants.SENDER_PASSWORD_IDENTIFIER))) {
-                entriesToBeModified.add(key);
-            } else if (key.equals(configMap.get(SMSConstants.SENDER_SECUREKEY_IDENTIFIER))) {
-                entriesToBeModified.add(key);
-            }
-        }
-        if (!CollectionUtils.isEmpty(entriesToBeModified)) {
-            for (String key : entriesToBeModified) {
-                if (key.equals(configMap.get(SMSConstants.SENDER_PASSWORD_IDENTIFIER))) {
-                    requestBody.remove(key);
-                    requestBody.add(key, encryptedPwd);
-                } else if (key.equals(configMap.get(SMSConstants.SENDER_SECUREKEY_IDENTIFIER))) {
-                    requestBody.remove(key);
-                    requestBody.add(key, hashMsg);
-                }
-            }
-        }
-    }
+		List<String> entriesToBeModified = new ArrayList<>();
+		for (String key : requestBody.keySet()) {
+			if (key.equals(configMap.get(SMSConstants.SENDER_PASSWORD_IDENTIFIER))) {
+				entriesToBeModified.add(key);
+			} else if (key.equals(configMap.get(SMSConstants.SENDER_SECUREKEY_IDENTIFIER))) {
+				entriesToBeModified.add(key);
+			}
+		}
+		if (!CollectionUtils.isEmpty(entriesToBeModified)) {
+			for (String key : entriesToBeModified) {
+				if (key.equals(configMap.get(SMSConstants.SENDER_PASSWORD_IDENTIFIER))) {
+					requestBody.remove(key);
+					requestBody.add(key, encryptedPwd);
+				} else if (key.equals(configMap.get(SMSConstants.SENDER_SECUREKEY_IDENTIFIER))) {
+					requestBody.remove(key);
+					requestBody.add(key, hashMsg);
+				}
+			}
+		}
+	}
 
-    /**
-     * A map to fetch the configured keys for attributes.
-     *
-     * @return
-     */
-    public Map<String, String> getConfigMap() {
-        Map<String, String> configMap = new HashMap<>();
-        for (String key : smsProperties.getConfigMap().keySet()) {
-            String value = smsProperties.getConfigMap().get(key);
-            if (value.contains("$")) {
-                if (value.equals("$username"))
-                    configMap.put(SMSConstants.SENDER_USERNAME_IDENTIFIER, key);
-                else if (value.equals("$password"))
-                    configMap.put(SMSConstants.SENDER_PASSWORD_IDENTIFIER, key);
-                else if (value.equals("$senderid"))
-                    configMap.put(SMSConstants.SENDER_SENDERID_IDENTIFIER, key);
-                else if (value.equals("$secureKey"))
-                    configMap.put(SMSConstants.SENDER_SECUREKEY_IDENTIFIER, key);
-                else if (value.equals("$mobileno"))
-                    configMap.put(SMSConstants.SENDER_MOBNO_IDENTIFIER, key);
-                else if (value.equals("$message"))
-                    configMap.put(SMSConstants.SENDER_MESSAGE_IDENTIFIER, key);
-                else if (value.equals("$templateid"))
-                	configMap.put(SMSConstants.TEMPLATE_ID, key);
-            }
-        }
-        configMap.put("smsservicetype", "singlemsg");
-        return configMap;
-    }
+	/**
+	 * A map to fetch the configured keys for attributes.
+	 *
+	 * @return
+	 */
+	public Map<String, String> getConfigMap() {
+		Map<String, String> configMap = new HashMap<>();
+		for (String key : smsProperties.getConfigMap().keySet()) {
+			String value = smsProperties.getConfigMap().get(key);
+			if (value.contains("$")) {
+				if (value.equals("$username"))
+					configMap.put(SMSConstants.SENDER_USERNAME_IDENTIFIER, key);
+				else if (value.equals("$password"))
+					configMap.put(SMSConstants.SENDER_PASSWORD_IDENTIFIER, key);
+				else if (value.equals("$senderid"))
+					configMap.put(SMSConstants.SENDER_SENDERID_IDENTIFIER, key);
+				else if (value.equals("$secureKey"))
+					configMap.put(SMSConstants.SENDER_SECUREKEY_IDENTIFIER, key);
+				else if (value.equals("$mobileno"))
+					configMap.put(SMSConstants.SENDER_MOBNO_IDENTIFIER, key);
+				else if (value.equals("$message"))
+					configMap.put(SMSConstants.SENDER_MESSAGE_IDENTIFIER, key);
+				else if (value.equals("$templateid"))
+					configMap.put(SMSConstants.TEMPLATE_ID, key);
+			}
+		}
+		configMap.put("smsservicetype", "singlemsg");
+		return configMap;
+	}
 
-    /**
-     * Hash generator
-     *
-     * @param userName
-     * @param senderId
-     * @param content
-     * @param secureKey
-     * @return
-     */
-    private String hashGenerator(String userName, String senderId, String content, String secureKey) {
-        StringBuffer finalString = new StringBuffer();
-        finalString.append(userName.trim()).append(senderId.trim()).append(content.trim()).append(secureKey.trim());
-        String hashGen = finalString.toString();
-        StringBuffer sb = null;
-        MessageDigest md;
-        try {
-            md = MessageDigest.getInstance("SHA-512");
-            md.update(hashGen.getBytes());
-            byte byteData[] = md.digest();
-            // convert the byte to hex format method 1
-            sb = new StringBuffer();
-            for (int i = 0; i < byteData.length; i++) {
-                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-            }
+	/**
+	 * Hash generator
+	 *
+	 * @param userName
+	 * @param senderId
+	 * @param content
+	 * @param secureKey
+	 * @return
+	 */
+	private String hashGenerator(String userName, String senderId, String content, String secureKey) {
+		StringBuffer finalString = new StringBuffer();
+		finalString.append(userName.trim()).append(senderId.trim()).append(content.trim()).append(secureKey.trim());
+		String hashGen = finalString.toString();
+		StringBuffer sb = null;
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("SHA-512");
+			md.update(hashGen.getBytes());
+			byte byteData[] = md.digest();
+			// convert the byte to hex format method 1
+			sb = new StringBuffer();
+			for (int i = 0; i < byteData.length; i++) {
+				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+			}
 
-        } catch (Exception e) {
-            log.error("Exception while generating the hash: ", e);
-        }
-        return sb.toString();
-    }
+		} catch (Exception e) {
+			log.error("Exception while generating the hash: ", e);
+		}
+		return sb.toString();
+	}
 
 }
