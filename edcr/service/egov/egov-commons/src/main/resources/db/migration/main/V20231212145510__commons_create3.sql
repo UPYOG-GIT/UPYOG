85090106bcbff81fb82bc98f5571de3fd74c40f7
UPYOG-GIT <support@entitcs.com>
2023-12-30 11:44:03 +0530

    edcr

--- edcr/service/egov/egov-commons/src/main/resources/db/migration/main/V20231212145510__commons_create3.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE IF NOT EXISTS edcr_rule_entry
-ADD COLUMN development_zone character varying(64),
-ADD COLUMN road_width numeric(5,2),
-ADD COLUMN no_of_floors int,
-ADD COLUMN depth_width numeric(5,2);
-
