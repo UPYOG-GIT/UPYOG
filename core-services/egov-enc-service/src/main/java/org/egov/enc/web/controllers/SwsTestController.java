40a30a49fcac1a5cfd5d548592d9a1aa3b8d9e4c
UPYOG-GIT <support@entitcs.com>
2024-05-30 12:42:59 +0530

    Enc Service changes

--- core-services/egov-enc-service/src/main/java/org/egov/enc/web/controllers/SwsTestController.java
+++ core-services/egov-enc-service/src/main/java/org/egov/enc/web/controllers/SwsTestController.java
@@ -3,6 +3,7 @@ package org.egov.enc.web.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -41,6 +42,38 @@ public class SwsTestController {
 
 		log.info("requestEntity : " + requestEntity.toString());
 		String apiUrl = "https://industries.cg.gov.in/swschhattisgarhserviceapi/api/SwsService/getswsprofile";
+
+		try {
+			// Make the API call using RestTemplatex1x
+			ResponseEntity<String> response = restTemplate.exchange(apiUrl, HttpMethod.POST, requestEntity,
+					String.class);
+
+			log.info("response " + response.toString());
+			return ResponseEntity.ok(response.getBody().toString());
+		} catch (Exception ex) {
+			String error = "Error: " + ex;
+			log.error("Error: " + error);
+			return ResponseEntity.ok(error);
+		}
+	}
+	
+	
+	@PostMapping("/_getapplicationdetails")
+	public ResponseEntity<String> getApplicationDetails(@RequestBody JSONObject data) {
+
+		Map<String, Object> requestBody = new HashMap<>();
+//		requestBody.put("applicationNumber", "112405250417235");
+		requestBody.put("applicationNumber", data.get("applicationNumber").toString());
+		HttpHeaders headers = new HttpHeaders();
+//		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
+		headers.setBearerAuth(data.get("swsAuthToken").toString());
+		headers.set("swskey", "5227439299922");
+
+		HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
+
+		log.info("requestEntity : " + requestEntity.toString());
+		String apiUrl = "https://industries.cg.gov.in/swschhattisgarhserviceapi/api/UnitRegistration/getapplicationdetail";
 
 		try {
 			// Make the API call using RestTemplatex1x
