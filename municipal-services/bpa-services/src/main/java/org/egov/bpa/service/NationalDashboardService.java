6c0810c0bfa4a1f8235930e3e8373a72e6d01c0c
bhupeshentit <bhupesh.dewangan@entitcs.com>
2024-10-24 12:54:37 +0530

    Sheduler start for national dashboard

--- municipal-services/bpa-services/src/main/java/org/egov/bpa/service/NationalDashboardService.java
+++ municipal-services/bpa-services/src/main/java/org/egov/bpa/service/NationalDashboardService.java
@@ -619,7 +619,7 @@ public class NationalDashboardService {
 
 	}
 
-//	@Scheduled(cron = "0 58 23 * * *")
+	@Scheduled(cron = "0 58 23 * * *")
 	public void scheduleDataPush() {
 
 		log.info("Scheduled task started...");
