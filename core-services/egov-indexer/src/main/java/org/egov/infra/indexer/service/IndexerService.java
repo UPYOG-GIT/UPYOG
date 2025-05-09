beb51d79f3083b32af4b76998546afc6327c003a
bhupeshentit <bhupesh.dewangan@entitcs.com>
2025-05-09 14:30:08 +0530

    Indexer Service changes

--- core-services/egov-indexer/src/main/java/org/egov/infra/indexer/service/IndexerService.java
+++ core-services/egov-indexer/src/main/java/org/egov/infra/indexer/service/IndexerService.java
@@ -132,11 +132,14 @@ public class IndexerService {
 			jsonToBeIndexed = dataTransformationService.buildJsonForIndex(index, kafkaJson, isBulk, false);
 		}
 
-		if(index.getName().contains("collection") || index.getName().contains("payment") || configkey.equals(Mapping.ConfigKeyEnum.LEGACYINDEX)) {
-			// this is already sent
-		} else {
-			validateAndIndex(jsonToBeIndexed, url.toString(), index);
-		}
+		/*
+		 * if(index.getName().contains("collection") ||
+		 * index.getName().contains("payment") ||
+		 * configkey.equals(Mapping.ConfigKeyEnum.LEGACYINDEX)) { // this is already
+		 * sent } else { validateAndIndex(jsonToBeIndexed, url.toString(), index); }
+		 */
+		
+		validateAndIndex(jsonToBeIndexed, url.toString(), index);
 
 		log.info("Total time taken: " + ((new Date().getTime()) - startTime) + "ms");
 	}
