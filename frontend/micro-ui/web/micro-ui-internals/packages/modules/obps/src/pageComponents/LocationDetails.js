3d2a8acc09592c37dfda77e3ba13ac957e00c0fc
bhupeshentit <bhupesh.dewangan@entitcs.com>
2025-11-04 16:20:37 +0530

    Digit-Ui: enable GIS on bpa module for citya

--- frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pageComponents/LocationDetails.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/modules/obps/src/pageComponents/LocationDetails.js
@@ -31,7 +31,8 @@ const LocationDetails = ({ t, config, onSelect, userType, formData, ownerIndex =
   let cityCode = formData?.data?.edcrDetails?.tenantId;
   formData = { address: { ...formData?.address } };
   const enabledTenantIds = globalConfigs?.getConfig("GIS_ENABLED");
-  const isGISEnabled = Array.isArray(enabledTenantIds) && enabledTenantIds.includes(cityCode);
+  // const isGISEnabled = Array.isArray(enabledTenantIds) && enabledTenantIds.includes(cityCode);
+  const isGISEnabled = cityCode === "cg.citya" ? true : false;
 
   // console.log("enabledTenantIds: " + enabledTenantIds);
   // console.log("aaaaa "+Array.isArray(enabledTenantIds));
